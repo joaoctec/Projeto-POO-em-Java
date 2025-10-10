a6b9f8f754f569295c1480ae84b27b3c28a2b9dd
J0𝔸0 _ 𝒞ésar-ⓦeb <joaocucb@gmail.com>
2025-10-10 10:38:48 -0300

    Salnavdo duas atividades denovo

--- z2SistemaRG/src/br/com/rh/classes/Funcionario.java
+++ z2SistemaRG/src/br/com/rh/classes/Funcionario.java
@@ -1,5 +1,11 @@
 package br.com.rh.classes;
 
 public abstract class Funcionario {
-
+	
+	private String nome;
+	private double salario;
+	
+	public abstract void aumentarSalario(); 
+	
+	
 }
--- z2SistemaRG/src/br/com/rh/classes/Gernte.java
+++ z2SistemaRG/src/br/com/rh/classes/Gernte.java
@@ -1,5 +1,9 @@
 package br.com.rh.classes;
 
-public class Gernte {
+public class Gernte extends Funcionario {
+	
+	public void auemntarSalario() {
+		double salario = 1000 * 0.10;
+	}
 
 }
